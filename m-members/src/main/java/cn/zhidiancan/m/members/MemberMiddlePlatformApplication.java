c371e0b68037c2b7153ab39396c1ad9c289939c3
supnumb <supnumb@163.com>
2020-12-07 22:04:58 +0800

    提交代码到仓库

---
+++ m-members/src/main/java/cn/zhidiancan/m/members/MemberMiddlePlatformApplication.java
@@ -0,0 +1,15 @@
+package cn.zhidiancan.m.members;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MemberMiddlePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberMiddlePlatformApplication.class, args);
+	}
+
+}
+
---
+++ m-members/src/test/java/cn/zhidiancan/m/members/MemberMiddlePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zhidiancan.m.members;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberMiddlePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
